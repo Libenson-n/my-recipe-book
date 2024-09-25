09fb9b6c5e851eea227da6c6b3ce8ac1a6a2c3ee
Libenson Nicolas <libenson.n@gmail.com>
2024-09-25 10:09:45 -0400

    seeded database

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,4 +1,5 @@
 const Hero = () => {
+  
   return (
     <div className="h-[40vh] min-h-1/3 bg-[url(https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=1153&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat">
       <div className="bg-black/70">
@@ -8,6 +9,7 @@ const Hero = () => {
         <p className="text-3xl text-white">
           Share your favorite recipes & discover new ones.
         </p>
+       
       </div>
     </div>
   );
